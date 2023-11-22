cd15f5bbf410d4ece9a95bd37466faf1f65b2168
Ashish Baravaliya <vier.institute35@gmail.com>
2023-11-22 16:05:54 -0500

    build test

--- api/hello.ts
+++ api/hello.ts
@@ -2,6 +2,6 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   return res.json({
-    message: `Hello from travis ci!`,
+    message: `Hello from travis ci!ww`,
   });
 }
